6a0b29e699a81505dcc3dd6295a19c3e26903155
vinhuyen <vinhuyen.theonly@gmail.com>
2022-05-10 11:04:03 +0700

    change something

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React,{Component} from "react";
 import "./assets/css/macbookStyle.css"
+// import React, { useState } from 'react';
 
+// function StepTracker() {
+//     const [steps, setSteps] = useState(0);
+//
+//     function changeColor(){
+//         setSteps()
+//     }
+//
+//     function increment() {
+//         setSteps(prevState => prevState + 1);
+//     }
+//
+//     return (
+//         <div>
+//             Today you've taken {steps} steps!
+//             <br />
+//             <button onClick={increment}>
+//                 I took another step
+//             </button>
+//         </div>
+//     );
+// }
+
+
+// function App() {
+//     const [count, setCount] = useState(3);
+//     return (
+//         <div>
+//             <p>You clicked {count} times</p>
+//             <button onClick={() => setCount(count + 1)}>
+//                 Click me
+//             </button>
+//         </div>
+//     );
+// }
 class App extends Component {
     constructor(props) {
         super(props);
         this.state ={
             items:[],
             isLoaded: false,
+            categoryID: 1,
         }
     }
 
@@ -21,49 +57,41 @@ class App extends Component {
             })
     }
 
-    // callAPI(){
-    //     this.setState({
-    //         isLoaded: true,
-    //     });
-    //     fetch("https://625bcc9a398f3bc782aed2bb.mockapi.io/products")
-    //         .then(res => res.json())
-    //         .then(
-    //             (result) => {
-    //                 this.setState({
-    //                     isLoaded: false,
-    //                     items: result.items
-    //                 });
-    //             }
-    //         )
-    // }
-
     render() {
-        const {isLoaded, items} = this.state;
+        const {isLoaded, items, categoryID} = this.state;
 
         if(!isLoaded){
             return <div>Loading...</div>;
         }
         return (
             <div>
-                <h2>MACBOOK SIU XỊN</h2>
+                <h2>CHON LOAI HANG</h2>
+                    <section>
+                        <button onClick={() => {this.setState({...this.state,categoryID: 1})}}>macbook</button>
+                        <button onClick={() => {this.setState({...this.state,categoryID: 3})}}>ipad</button>
+                        <button onClick={() => {this.setState({...this.state,categoryID: 2})}}>iphone</button>
+                    </section>
                 <div className="wrapper">
-                    {/* eslint-disable-next-line array-callback-return */}
-                    {items.map(item => {
-                        return(
-                            <div className="macbook__cart" key={item.id}>
-                                <img src={item.image} alt=""/>
-                                <p>{item.title}</p>
-                                <p>{item.price}</p>
-                                <p>{item.offer}</p>
-                            </div>
+                    {items.filter(item => item.categoryID == categoryID)
+                        .map(item =>{
+                                return(
+                                    <div className="macbook__cart">
+                                        <img src={item.image} alt=""/>
+                                        <h3>{item.title}</h3>
+                                        <p>{item.price}</p>
+                                        <p>{item.offer}</p>
+                                    </div>
+                                )
+                            }
                         )
-                    })}
+                    }
                 </div>
             </div>
 
         );
     }
 }
-
-
 export default App;
+
+// export default StepTracker;
+
